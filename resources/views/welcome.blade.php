0a82d2f0b1063dd3bd5f1ceb02f853b095cc66fa
Ali Essa <106039381+AliEssa118658@users.noreply.github.com>
2023-10-20 17:41:19 +0300

    changes design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
 
             </div>
             <div class="col-md-6  mt-5">
-                <img src="/images/svg/large-logo.svg" alt="" class="d-block m-auto large-logo" style="width:60%" >
+                <img src="/images/svg/large-logo.svg?" alt="" class="d-block m-auto large-logo" style="width:60%" >
                 <a href="{{ route('news', ['news' => 5]) }}" class="learn-more-p">{{__('Learn More')}}</a>
             </div>
         </div>
